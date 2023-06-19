a4d2537a86a2b2d4a1429168adb1bce7c08b2cd8
Fréederic Baudet <baudet_f@yahoo.fr>
2023-06-19 17:02:12 +0200

    Seedz

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,18 @@
+#  To be checked: One user cannot book his own offer
+# class UserValidator < ActiveModel::Validator
+
+  #   def validate(record)
+#     if record.user == record.offer.user
+#       record.errors.add :base, "Cannot book your own offer!"
+#     end
+#   end
+# end
+
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :offer
   # Comments: blank at the moment, to be tested
   # To be tested: A user can only book the same offer one time (one more time?!?)
   validates :user, uniqueness: { scope: :offer }
+  # validates_with UserValidator // see above
 end
