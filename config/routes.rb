c0b27b463317885885eaa317158b0c68f7a359bd
paulderuelle <p.deruelle.mail@gmail.com>
2023-06-20 15:18:32 +0200

    homepage corrected

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :offers do
     resources :reservations, only: %i[new create edit update]
   end
   resources :reservations, only: %i[index destroy]
+
+  root to: "offers#index"
 end
