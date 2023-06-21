e36ff64e5605bd026bd29f76f7857d4bd429f014
paulderuelle <p.deruelle.mail@gmail.com>
2023-06-21 12:42:47 +0200

    fixed logo, background & index

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,7 +2,7 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
   # Uniqueness: to be tested
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true, length: { maximum: 30 }
   validates :description, presence: true
   validates :date, presence: true
   validates :price, presence: true
